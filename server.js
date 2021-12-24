b9189b1e11073bff66172045520049549999ad21
williamc160 <williamcann160@hotmail.com>
2021-12-24 00:42:42 +0000

    adding server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var requests=0;
+var podname= process.env.HOSTNAME;
+var startTime;
+var host;
+
+var handleRequest = function(request, response) {
+  response.setHeader('Content-Type', 'text/plain');
+  response.writeHead(200);
+  response.write("DevOps Coursework 2! | Running on: ");
+  response.write(host);
+  response.end(" | v=0\n");
+  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptim$
+
+var www = http.createServer(handleRequest);
+www.listen(8080,function () {
+    startTime = new Date();;
+    host = process.env.HOSTNAME;
+    console.log ("Started At:",startTime, "| Running On: " ,host, "\n" );
+});
+
